6bfe832541252f1c1348258808ea587192f13849
Charlie Mills <charlie@computerbacon.com>
2014-04-21 03:58:59 +0100

    Replaced Queue and Playlist with subclasses of collections.MutableSequence.

---
+++ partybox/media.py
@@ -0,0 +1,175 @@
+import random
+import collections
+import abc
+
+class MediaType(object):
+    LOCAL = 1
+    STREAM = 2
+    RADIO = 3
+
+class AbstractMedia(object):
+    """
+    Abstract base class for Media objects.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+
+    @abc.abstractproperty
+    def length(self):
+        """
+        Duration of media in seconds.
+        """
+        pass
+
+    @abc.abstractproperty
+    def artist(self):
+        """
+        Artist/Producer of media.
+        """
+        pass
+
+    @abc.abstractproperty
+    def title(self):
+        """
+        Title or name of media.
+        """
+        pass
+
+    @abc.abstractproperty
+    def album(self):
+        """
+        Album the media belongs to.
+        """
+        pass
+
+    @abc.abstractproperty
+    def artwork(self):
+        """
+        URI to track artwork.
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_uri(self):
+        """
+        Returns the URI for the media, this can be a one time URI.
+
+        """
+        pass
+
+
+class BaseList(collections.MutableSequence):
+    """
+    Base wrapper class for list, subclass to create
+    sequence like objects.
+    """
+    def __init__(self):
+        self._container = list()
+
+    def __getitem__(self, item):
+        return self._container.__getitem__(item)
+
+    def __setitem__(self, key, value):
+        return self._container.__setitem__(key, value)
+
+    def __delitem__(self, key):
+        return self._container.__delitem__(key)
+
+    def __len__(self):
+        return self._container.__len__()
+
+    def __str__(self):
+        return self._container.__str__()
+
+    def insert(self, index, value):
+        return self._container.insert(index, value)
+
+
+class PlayList(BaseList):
+    """
+    An ordered collection of persisted Media objects.
+    """
+
+    def shuffle(self):
+        """
+        Shuffle the order of the playlist
+        """
+        random.shuffle(self._container)
+
+    @property
+    def duration(self):
+        """
+        The sum of media duration.
+        """
+        #TODO: This should possibly be a method.
+        i = 0
+        for media in self._container:
+            i += media.length
+        return i
+
+    @property
+    def play_count(self):
+        """
+        How many times the playlist has been played
+        """
+        raise NotImplementedError()
+
+    @property
+    def date_created(self):
+        """
+        Date of creation.
+        """
+        raise NotImplementedError()
+
+    @property
+    def date_edited(self):
+        """
+        Date of last edit.
+        """
+        raise NotImplementedError()
+
+
+class Queue(BaseList):
+    """
+    A queue of Media objects, used to manage 'Now Playing'
+    items.
+    """
+
+    @property
+    def up_next(self):
+        """
+        The next item in the queue.
+        """
+        return self._container[0]
+
+    def get(self):
+        """
+        Remove and return the next item from the queue.
+        """
+        return self._container.pop(0)
+
+    def clear(self):
+        """
+        Clear/empty the queue
+        """
+        self._container = list()
+
+    def shuffle(self):
+        """
+        Shuffles the order of the queue
+        """
+        random.shuffle(self._container)
+
+    def load_playlist(self, playlist):
+        """
+        Clear the queue and load the contents of a playlist.
+        """
+        self.clear()
+        self.extend(playlist)
+
+
+class CollaborativeQueue(Queue):
+
+    def __init__(self):
+        raise NotImplementedError()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,34 +1,34 @@
 import unittest
-from partybox import queue
-import warnings
+from partybox import media
 
-class StandardQueueTest(unittest.TestCase):
+
+class QueueTest(unittest.TestCase):
 
     def setUp(self):
-        self.queue = queue.StandardQueue()
+        self.queue = media.Queue()
 
     def test_core(self):
-        self.queue.add("track1.mp3")
-        self.assertIn("track1.mp3", self.queue._queue)
-        warnings.warn("StandardQueue does not implement container methods")
+        self.queue.append("track1.mp3")
+        self.assertIn("track1.mp3", self.queue)
 
         self.queue.remove("track1.mp3")
-        self.assertNotIn("track1.mp3", self.queue._queue)
+        self.assertNotIn("track1.mp3", self.queue)
+        print(len(self.queue))
         self.assertEqual(0, len(self.queue))
 
         for i in range(0, 10):
-            self.queue.add("track{}.mp3".format(i))
+            self.queue.append("track{}.mp3".format(i))
 
         self.assertEqual(10, len(self.queue))
-        self.assertEqual(self.queue.next, "track1.mp3")
+        self.assertEqual(self.queue.up_next, "track0.mp3")
 
-        self.queue.add_next("oddball.mp3")
-        self.assertEqual(self.queue.next, "oddball.mp3")
+        self.queue.insert(0, "oddball.mp3")
+        self.assertEqual(self.queue.up_next, "oddball.mp3")
 
     def test_shuffle(self):
         for i in range(0, 10):
-            self.queue.add("track{}.mp3".format(i))
-        unshuffled = list(self.queue._queue) #Make a copy
-        self.queue.toggle_shuffle()
-        self.assertNotEqual(unshuffled, self.queue._queue)
+            self.queue.append("track{}.mp3".format(i))
+        unshuffled = list(self.queue) #Make a copy
+        self.queue.shuffle()
+        self.assertNotEqual(unshuffled, self.queue)
 
